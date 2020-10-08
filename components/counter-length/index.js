6c0a89aa7255f203c8709c05c976e55f2002ab21
AdrianGD1337 <contacto.plazmedia@gmail.com>
2020-10-08 18:05:04 -0500

    [TEST] Contador de caracteres estilo Twitter

---
+++ components/counter-length/index.js
@@ -0,0 +1,102 @@
+import { useEffect, useState } from 'react';
+
+export default function CounterLength({ characters = 0 }){
+
+  const [resCharacters, setResCharacters] = useState(null);
+
+  // Equivale al 100% de caracteres validos para el Devit
+  const charactersAvailables = 280; // => 100%
+
+  // Porcentaje de caracteres ocupados
+  let progress = characters * 100 / charactersAvailables;
+  
+  let compensation = progress >= 90 ? 2 : 10;
+  
+  let primaryColor = progress >= 90 ? '#FFAD1F' : '#1da1f2';
+  let secondaryColor = '#ffffff';
+  let textColor = progress >= 100 ? '#E0245E' : '#ffffff';
+  let opacity = .3; 
+  let stroke= 2;
+  let size = 35;
+
+  if(progress >= 100){
+    primaryColor = '#E0245E';
+  }
+
+  // const size = radius * 2;
+  const subtractionSize = size - stroke * 2 - compensation;
+  const circumference = subtractionSize * Math.PI;
+  let percentage = circumference - progress / 100 * circumference;
+
+  const addOpacityToColor = () => {
+    const opacityHex = Math.round(opacity * 255).toString(16)
+    return `${secondaryColor}${opacityHex}`
+  };
+
+  useEffect(() => {
+    if(progress >= 90){
+      setResCharacters(charactersAvailables - characters);
+    }else{
+      setResCharacters(null);
+    };
+  }, [progress]);
+
+  return (
+    <>
+      <section>
+        <svg
+          height={size}
+          width={size}
+        >
+          <circle
+            stroke={addOpacityToColor()}
+            strokeWidth={stroke}
+            fill="transparent"
+            r={ subtractionSize / 2 }
+            cx="50%"
+            cy="50%"
+          />
+          <circle
+            stroke={primaryColor}
+            strokeWidth={stroke}
+            fill="transparent"
+            r={ subtractionSize / 2 }
+            cx="50%"
+            cy="50%"
+            strokeDasharray={`${circumference} ${circumference}`}
+            strokeDashoffset={ progress < 100 ? Number(percentage) : circumference + circumference }
+          />
+        </svg>
+        <div>
+          <p>{ resCharacters }</p>
+        </div>
+      </section>
+      <style jsx>{`
+        section{
+          position: relative;
+          display: inline-block;
+        }
+        circle{
+          transition: all 0.35s; 
+          transform: rotate(-90deg);
+          transform-origin: 50% 50%;
+        }
+        div{
+          position: absolute;
+          height: ${size}px;
+          width: ${size}px;
+          left: 0;
+          top: 0;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+        p{
+          font-size: calc(${size}px / 3);
+          font-weight: lighter;
+          color: ${textColor};
+        }
+      `}</style>
+    </>
+  );
+};
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,16 +1,32 @@
+import { useState } from 'react';
 import Layount from "components/layout";
 import NavBar from "components/nav-bar.js/nav-bar";
 import Header from "components/header/header";
+import CounterLength from "components/counter-length";
 
 export default function Home(){
+
+  const [characters, setCharacters] = useState(0);
+
   return (
     <>
       <Layount>
 
         <Header/>
 
-        <section className="overflow-x-auto">
-          <div className="h-screen bg-blue-500 block"></div>
+        <section className="overflow-x-hidden min-h-full pla">
+          <div className="p-4">
+            <textarea 
+              name="devit"
+              id="devit"
+              rows="5"
+              placeholder="En que estas pensando"
+              className="w-full box-border resize bg-transparent text-white placeholder-gray-600"
+              onChange={ e => setCharacters(e.target.value.length) }
+            ></textarea>
+          </div>
+          {/* <div className="h-screen bg-blue-500 block"></div> */}
+          <CounterLength characters={ characters }/>
         </section>
 
         <NavBar/>
@@ -18,7 +34,13 @@ export default function Home(){
       </Layount>
 
       <style jsx>{`
-         
+         section{
+           min-height: calc(100% - 112px);
+           background-color: #15202b;
+         }
+         div{
+           background-color: #253341;
+         }
       `}</style>
     </>
   );
